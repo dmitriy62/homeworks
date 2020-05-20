3c92067c454217a7d3d263bb0ae43704ae028b5c
dmitriy62 <molotkov.dima2001@gmail.com>
2020-05-20 12:33:47 +0400

    1-я домашняя работа

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+var name = prompt ('Ваше имя ?');
+str1 = 'Привет, мир! Меня зовут ';
+alert (str1 + name);
+var a = 4 ;
+var b = 3 ;
+var c = 4 ;
+var s = ((a + b + c)/3);
+alert ('Средняя оценка: ' + s);
